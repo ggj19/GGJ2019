0ebc8fb94f9be391db6e4498fb1d03bd3405baaf
jihoon <hun2ya9@naver.com>
2019-01-26 06:08:09 +0900

    UPDATE _ 발사체가 돌면서 날아가도록

--- GGJ2019/GGJ2019/Assets/Scripts/Projectile/Projectile.cs
+++ GGJ2019/GGJ2019/Assets/Scripts/Projectile/Projectile.cs
@@ -4,9 +4,29 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    private bool inCam;
+    private void OnBecameVisible() => inCam = true;
+    private void OnBecameInvisible() => inCam = false;
+
+    private Vector3 angle;
     private void Awake()
     {
-        if(gameObject)
+        angle = Vector3.forward * Random.Range(1, 10f);
+
+        if (gameObject)
             Destroy(gameObject, 10);
+    }
+    public void Update()
+    {
+        if (gameObject)
+        {
+            if (inCam)
+            {
+                gameObject.SetActive(true);
+                transform.Rotate(angle);
+            }
+            else
+                gameObject.SetActive(false);
+        }
     }
 }
